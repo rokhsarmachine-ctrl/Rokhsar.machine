029baf80149dfb95203290869c660962f8938b26
rokhsarmachine-ctrl <rokhsar.machine@gmail.com>
2026-09-11 17:15:23 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
+import os
 import telebot
 
-TOKEN = "8390893863:AAEZFhkYG0l22pWGNr3rounwkUChtxneOPc"
+TOKEN = os.getenv("8390893863:AAEZFhkYG0l22pWGNr3rounwkUChtxneOPc")
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "سلام عزیزم، ثبت سفارش ماشین سی ان سی اینجاست 🌸")
+ADMIN_ID = 7122529232  # اینجا آیدی عددی خودت را بگذار
 
-bot.polling()
+@bot.message_handler(func=lambda m: True)
+def forward_to_admin(message):
+    text = f"پیام جدید از @{message.from_user.username}:\n\n{message.text}"
+    bot.send_message(ADMIN_ID, text)
