44ce75feed2f98122f0dbc15add98ee2371e56d1
rokhsarmachine-ctrl <rokhsar.machine@gmail.com>
2026-09-11 17:32:44 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import telebot
+
 TOKEN = "8390893863:AAEZFhkYG0l22pWGNr3rounwkUChtxneOPc"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "سلام عزیزم، ثبت سفارش ماشین سی ان سی اینجاست 🌸")
+    bot.reply_to(message, "سلام عزیزم، ثبت سفارش ماشین اینجاست 🌸")
 
-
-ADMIN_ID = 7122529232
 bot.polling()
