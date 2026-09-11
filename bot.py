0df5bb48117079ef00b6ebc622fb53f997a536e1
rokhsarmachine-ctrl <rokhsar.machine@gmail.com>
2026-09-11 17:18:49 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+import telebot
+
+TOKEN = "8390893863:AAEZFhkYG0l22pWGNr3rounwkUChtxneOPc"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام عزیزم، ثبت سفارش ماشین سی ان سی اینجاست 🌸")
+
+bot.polling()
+
 import os
 import telebot
 
