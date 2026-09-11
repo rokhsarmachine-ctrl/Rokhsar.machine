d66c945c269e2ddcbe32aeda189276f2fde64872
rokhsarmachine-ctrl <rokhsar.machine@gmail.com>
2026-09-11 17:28:58 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,5 @@ def start(message):
     bot.reply_to(message, "سلام عزیزم، ثبت سفارش ماشین سی ان سی اینجاست 🌸")
 
 
-ADMIN_ID = 7122529232  # اینجا آیدی عددی خودت را بگذار
-
-@bot.message_handler(func=lambda m: True)
-def forward_to_admin(message):
-    text = f"پیام جدید از @{message.from_user.username}:\n\n{message.text}"
-    bot.send_message(ADMIN_ID, text)
+ADMIN_ID = 7122529232
 bot.polling()
